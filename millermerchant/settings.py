12f79617835e988e8f5309f1d22b55a702f26517
zanderm73 <zanderm73@hotmail.co.uk>
2020-02-16 21:53:28 +0000

    updated whitenoise code

--- millermerchant/settings.py
+++ millermerchant/settings.py
@@ -143,6 +143,7 @@ STATIC_URL = "/static/"
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
